ffd15daa32714ffde96e57df965b31d710e48102
sebassil12 <anddysilva15@gmail.com>
2023-12-02 20:17:23 -0500

    connection with mongodb, complete backend

---
+++ backend/demo/src/main/java/com/moviesapi/demo/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.moviesapi.demo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+    
+}
---
+++ frontend/movie-gold-v1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import api from './api/axios.config'
+
+function App() {
+  const [movie, setMovies] = useState()
+   
+
+  return (
+   
+  )
+}
+
+export default App
